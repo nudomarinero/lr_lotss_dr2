5309796924e44190ed30c670550cdcc62697d9a2
José Sabater Montes <jsm@iaa.es>
2019-10-16 17:12:31 +0100

    Download the tractor data

---
+++ scripts/data_download/legacy_download.py
@@ -0,0 +1,68 @@
+import os
+import urllib.request
+from astropy.table import Table
+from dotenv import load_dotenv, find_dotenv
+
+
+try:
+    BASEPATH = os.path.dirname(os.path.realpath(__file__))
+    data_path = os.path.join(BASEPATH, "..", "..", "data")
+except NameError:
+    if os.path.exists("data"):
+        BASEPATH = "."
+        data_path = os.path.join(BASEPATH, "data")
+    else:
+        BASEPATH = os.getcwd()
+        data_path = os.path.join(BASEPATH, "..", "..", "data")
+
+# Load environment config variables
+# https://saurabh-kumar.com/python-dotenv/
+load_dotenv(find_dotenv())
+LEGACY_DATA_PATH = os.getenv("LEGACY_DATA_PATH")
+
+template_url_n = "https://portal.nersc.gov/project/cosmo/data/legacysurvey/dr8/north/tractor/{}/tractor-{}.fits"
+template_url_s = "https://portal.nersc.gov/project/cosmo/data/legacysurvey/dr8/south/tractor/{}/tractor-{}.fits"
+
+## Aux
+def url_is_alive(url):
+    """
+    Checks that a given URL is reachable.
+    :param url: A URL
+    :rtype: bool
+    """
+    request = urllib.request.Request(url)
+    request.get_method = lambda: 'HEAD'
+
+    try:
+        urllib.request.urlopen(request)
+        return True
+    except urllib.request.HTTPError:
+        return False
+
+
+##
+brick_table = Table.read(os.path.join(data_path, 
+    "raw", "survey-bricks.fits.gz"))
+
+cond = ((brick_table["DEC"] <= 40) & 
+        (brick_table["DEC"] >= 17.5) &
+        ((brick_table["RA"] >= 324) | 
+         (brick_table["RA"] <= 51))
+)
+
+download_names = brick_table[cond]["BRICKNAME"][:]
+
+with open(os.path.join(BASEPATH, "download_legacy.sh"), 
+          "w") as out:
+    out.write("#!/bin/bash\n")
+    out.write("cd {}\n".format(LEGACY_DATA_PATH))
+    for name in download_names:
+        ra = name[:3]
+        #url_north = template_url_n.format(ra, name)
+        url_south = template_url_s.format(ra, name)
+        #print(name, 
+        #    url_is_alive(url_north), 
+        #    url_is_alive(url_south))
+        out.write("wget {}\n".format(url_south))
+        
+
