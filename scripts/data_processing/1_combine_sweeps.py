0b3253f8d4e5ff5fe9ca1dd0cc5dd3c714ce9934
José Sabater Montes <jsm@iaa.es>
2019-12-10 15:16:29 +0000

    Combine pipeline 1

---
+++ scripts/data_processing/1_combine_sweeps.py
@@ -0,0 +1,54 @@
+import os
+from glob import glob
+import numpy as np
+from astropy.table import Table, vstack
+from dotenv import load_dotenv, find_dotenv
+import astropy.units as u
+
+# Load environment config variables
+# https://saurabh-kumar.com/python-dotenv/
+load_dotenv(find_dotenv())
+LEGACY_DATA_PATH = os.getenv("LEGACY_DATA_PATH")
+UNWISE_DATA_PATH = os.getenv("UNWISE_DATA_PATH")
+
+try:
+    BASEPATH = os.path.dirname(os.path.realpath(__file__))
+    data_path = os.path.join(BASEPATH, "..", "..", "data")
+except NameError:
+    if os.path.exists("data"):
+        BASEPATH = "."
+        data_path = os.path.join(BASEPATH, "data")
+    else:
+        BASEPATH = os.getcwd()
+        data_path = os.path.join(BASEPATH, "..", "..", "data")
+cache_path = os.path.join(data_path, "catalogue_cache")
+
+
+bands_all = ['G', 'R', 'Z', 'W1', 'W2', 'W3', 'W4']
+sweep_columns = (
+    ['BRICKNAME', 'OBJID', 'TYPE'] +
+    ['RA', 'DEC', 'RA_IVAR', 'DEC_IVAR'] +
+    ['FLUX_{}'.format(b) for b in bands_all] + 
+    ['FLUXERR_{}'.format(b) for b in bands_all] +
+    ['ANYMASK_G', 'ANYMASK_R', 'ANYMASK_Z']
+    )
+
+output_path = os.path.join(LEGACY_DATA_PATH, "combined_sweep")
+sweep_path = os.path.join(LEGACY_DATA_PATH, "sweeps")
+
+list_sweeps = glob(os.path.join(sweep_path, "*.fits"))
+
+print("Load", list_sweeps[0])
+t0 = Table.read(list_sweeps[0])[sweep_columns]
+for sweep in list_sweeps[1:]:
+    print("Load", sweep)
+    t = Table.read(sweep)[sweep_columns]
+    print("Combine")
+    t0 = vstack([t0, t])
+del t
+
+os.makedirs(output_path, exist_ok=True)
+output_name = os.path.join(output_path, "master_sweep.fits")
+
+print("Start output")
+t0.write(output_name)
