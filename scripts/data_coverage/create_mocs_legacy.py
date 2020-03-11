441ccbcabeb85af83c815a3e435ece1de9618912
José Sabater Montes <jsm@iaa.es>
2020-03-11 10:57:21 +0000

    Quick version (not working)

--- scripts/data_coverage/create_mocs_legacy.py
+++ scripts/data_coverage/create_mocs_legacy.py
@@ -1,9 +1,10 @@
 import os
-from astropy.table import Table
+from astropy.table import Table, join
 from astropy.coordinates import SkyCoord, Angle
 import astropy.units as u
 from mocpy import MOC
 import numpy as np
+from tqdm import tqdm
 
 BASEPATH = os.path.dirname(os.path.realpath(__file__))
 data_path = os.path.join(BASEPATH, "..", "..", "data")
@@ -17,41 +18,46 @@ brick_table_north = Table.read(os.path.join(data_path,
 brick_table_south = Table.read(os.path.join(data_path, 
     "raw", "survey-bricks-dr8-south.fits.gz"))
 
-brick_mocs_south = []
-brick_mocs_north = []
+brick_table.rename_column("BRICKNAME", "brickname") 
+bricks_north = join(
+    brick_table_north, 
+    brick_table, 
+    join_type="left", 
+    keys="brickname")[["RA1", "DEC1", "RA2", "DEC2"]]
+bricks_south = join(
+    brick_table_south, 
+    brick_table, 
+    join_type="left", 
+    keys="brickname")[["RA1", "DEC1", "RA2", "DEC2"]]
 
-print(len(brick_table))
-for row in brick_table:
-#    if int((row["RA1"]-row["RA2"])*10000)%3600000 != 0:
-    if ((row["BRICKNAME"] in brick_table_north["brickname"]) or
-        (row["BRICKNAME"] in brick_table_south["brickname"])):
-        brick_vertices = [
+def moc_from_row(row):
+    """Create a MOC from a row with coordinates"""
+    brick_vertices = [
             (row["RA1"], row["DEC1"]), 
             (row["RA1"], row["DEC2"]), 
             (row["RA2"], row["DEC2"]), 
             (row["RA2"], row["DEC1"]),
         ]
-        vertices = SkyCoord(brick_vertices, unit="deg", frame="icrs")
-        brick_moc = MOC.from_polygon_skycoord(vertices, max_depth=9)
-        if row["BRICKNAME"] in brick_table_north["brickname"]:
-            brick_mocs_north.append(brick_moc)
-        if row["BRICKNAME"] in brick_table_south["brickname"]:
-            brick_mocs_south.append(brick_moc)
+    vertices = SkyCoord(brick_vertices, unit="deg", frame="icrs")
+    return MOC.from_polygon_skycoord(vertices, max_depth=10)
 
-brick_moc_north = brick_mocs_north[0]
-for i in range(1, len(brick_mocs_north)):
-    brick_moc_north += brick_mocs_north[i]
-brick_moc_north.normalize()
+print("North:", len(bricks_north))
+brick_moc_north = moc_from_row(bricks_north[0])
+for row in tqdm(bricks_north[1:]):
+    brick_moc = moc_from_row(row)
+    brick_moc_north = brick_moc_north.union(brick_moc)
 
-brick_moc_south = brick_mocs_south[0]
-for i in range(1, len(brick_mocs_south)):
-    brick_moc_south += brick_mocs_south[i]
-brick_moc_south.normalize()
+print("South:", len(bricks_south))
+brick_moc_south = moc_from_row(bricks_south[0])
+for row in tqdm(bricks_south[1:]):
+    brick_moc = moc_from_row(row)
+    brick_moc_south = brick_moc_south.union(brick_moc)
 
-brick_moc_north.write("moc_north.moc")
+brick_moc_north.write("moc_north.moc", format="fits")
+brick_moc_south.write("moc_south.moc", format="fits")
+
+brick_moc_north.write("moc_north.moc.json", format="json")
+brick_moc_south.write("moc_south.moc.json", format="json")
+
 brick_moc_north.write("moc_north.moc.fits")
-brick_moc_north.write("moc_north.moc.json")
-
-brick_moc_north.write("moc_north.moc")
-brick_moc_north.write("moc_north.moc.fits")
-brick_moc_north.write("moc_north.moc.json")
+brick_moc_south.write("moc_south.moc.fits")
